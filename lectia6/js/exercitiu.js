e66fd381caa48010f82f718c8f4df8a7bb68085e
nitaconstantin <ctinnita88@gmail.com>
2023-05-29 19:50:04 +0300

    lectia 6 - exercitiu v2

--- lectia6/js/exercitiu.js
+++ lectia6/js/exercitiu.js
@@ -28,3 +28,6 @@ function addValue() {
     }
   }
 }
+let x = "5";
+x++;
+console.log(x);
---
+++ lectia6/js/exercitiu_v2.js
@@ -0,0 +1,41 @@
+function addData() {
+  //   console.log("OK!");
+
+  const userData = prompt(
+    'Introduceti un nume, un numar sau textul "adevarat" sau "fals"'
+  );
+
+  let responseText = "";
+
+  const x = parseInt(userData);
+
+  if (userData === "") {
+    responseText = "Nu ai introdus nici o valoare. Mai incearca!";
+  } else if (isNaN(x)) {
+    if (userData === "adevarat" || userData === "fals") {
+      responseText = "Ai dreptate poate fi adevarat sau false";
+    } else {
+      responseText = "Salut " + userData + ". Bine te-am gasit!";
+    }
+  } else {
+    let y = 3;
+    let z = 20;
+    let res;
+    switch (true) {
+      case x >= 5 && x <= 10:
+        res = x * y;
+        responseText = x + " * " + y + " = " + res;
+        break;
+      case x > 10 && x <= 20:
+        res = x + z;
+        responseText = x + " + " + z + " = " + res;
+        break;
+      default:
+        responseText = "Trebuie sa alegi un numar intre 5 si 20";
+        break;
+    }
+  }
+
+  const dataDiv = document.getElementById("dataDiv");
+  dataDiv.innerText = responseText;
+}
