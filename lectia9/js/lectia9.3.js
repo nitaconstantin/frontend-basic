563f6a5a139a61b8e3c8cfa132d0bed78559946c
nitaconstantin <ctinnita88@gmail.com>
2023-06-28 19:43:28 +0300

    Lectia 9 - Obiecte - Math

--- lectia9/js/lectia9.3.js
+++ lectia9/js/lectia9.3.js
@@ -32,3 +32,26 @@ myFunc1.apply(personObjOne, paramsArr);
 
 let boundFunc = myFunc1.bind(personObjTwo);
 console.log(boundFunc("patinoar", "Maria"));
+
+let number1 = 9;
+let number2 = -3.5423;
+
+// Math.PI =
+console.log(typeof number1, typeof number2);
+console.log(Math.PI, Math.E);
+
+console.log(Math.sqrt(number1));
+
+function randomNumber(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+// console.log(Math.random());
+console.log(randomNumber(2, 7));
+
+console.log(Math.floor(number2));
+console.log(Math.ceil(number2));
+console.log(`Round: ${Math.round(7.4756)} -> 7, ${Math.round(7.68568)} -> 8`);
+
+console.log("Pow:", Math.pow(5, 3), 5 ** 3);
+
+console.log("Abs: ", Math.abs(-4));
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # https://www.bestcssbuttongenerator.com/
+
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
