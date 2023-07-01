18fb96f9b5b792fb0974f6048763ab9bf121db48
nitaconstantin <ctinnita88@gmail.com>
2023-07-01 23:07:19 +0300

    lectia 9 - metode de manipulare DOM - partea 1

--- lectia10/js/lectia10.1.js
+++ lectia10/js/lectia10.1.js
@@ -19,3 +19,35 @@ console.log(date1);
 const date2 = new Date("1990-03-05 12:35");
 const options = { dateStyle: "long", timeStyle: "long" /*weekday: "long"*/ };
 console.log(date2.toLocaleString("ro-RO", options), date2.getTimezoneOffset());
+
+// document reprezinta DOM
+const div1 = document.getElementById("myDiv1");
+div1.innerText = "valoare de test";
+console.log(div1);
+
+const divs = document.getElementsByTagName("div");
+console.log(divs);
+divs[2].innerText = "o alta valoare de test";
+
+const elements = document.getElementsByClassName("class1");
+console.log("Elements:", elements);
+elements[0].style.backgroundColor = "red";
+elements[0].style.width = "100px";
+// elements[0].hidden = true;
+// elements[0].setAttribute("hidden", "true");
+
+const newDiv = document.createElement("div");
+newDiv.innerText = "new div creat";
+// document.body.appendChild(newDiv);
+divs[1].appendChild(newDiv);
+document.body.removeChild(elements[1]);
+
+document.write("text adaugat cu document.write");
+
+// document.anchors
+// document.forms <form>
+// console.log(document.URL);
+// document.head <head>
+// document.images
+// document.links
+// document.title
